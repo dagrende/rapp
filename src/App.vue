0ba4579238cdbca490fe528fc31f12e4482adbe9
dagrende <dag.rende@find-out.se>
2018-04-18 07:25:03 +0200

    edit fields for all event attrs, event uuids, larger font size

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,27 @@
         @click="selectEvent(events.indexOf(event)); $event.stopPropagation()"
         :class="{selected: i === selectedEventIndex}">
         <div class="row1">
-          <span class="event-type">{{event.type}}</span>
+          <input type="text" :value="event.type" @change="changeEvent(event, 'type', $event.target.value)"
+            :hidden="i != selectedEventIndex"
+            @focus="$event.target.select()">
+          <span :hidden="i == selectedEventIndex">{{event.type}}</span>
           <span class="filler"></span>
-          <span class="event-time">{{event.startTime.format('YY-MM-DD h:mm')}}</span>
+          <input type="text" :value="event.startTime.format('YY-MM-DD h:mm')"
+            @change="changeEvent(event, 'startTime', moment($event.target.value, 'YY-MM-DD h:mm'))"
+            :hidden="i != selectedEventIndex"
+            @focus="$event.target.select()">
+          <span :hidden="i == selectedEventIndex">{{event.startTime.format('YY-MM-DD h:mm')}}</span>
         </div>
         <div class="row2">
-          <input type="text" v-model="event.comment" :hidden="i != selectedEventIndex"
-            @click="$event.target.select()">
+          <input type="text"
+            :value="event.comment"
+            @change="changeEvent(event, 'comment', $event.target.value)"
+            :hidden="i != selectedEventIndex"
+            @focus="$event.target.select()">
           <span :hidden="i == selectedEventIndex">{{event.comment}}</span>
         </div>
+      </div>
+      <div class="event dummy-event">
       </div>
     </div>
     <div class="event-type-filter">
@@ -30,15 +42,16 @@
 
 <script>
   import moment from 'moment';
+  import uuidv4 from 'uuid/v4';
 
   export default {
     name: 'app',
     data () {
       return {
         events: [
-          {type: 'bbi', startTime: moment(), comment: 'hej du glade'},
-          {type: 'hem', startTime: moment(), comment: ''},
-          {type: 'bbi', startTime: moment(), comment: 'tag en spade'}
+          {id: uuidv4(), type: 'bbi', startTime: moment(), comment: 'hej du glade'},
+          {id: uuidv4(), type: 'hem', startTime: moment(), comment: ''},
+          {id: uuidv4(), type: 'bbi', startTime: moment(), comment: 'tag en spade'}
         ],
         eventType: '',
         selectedEventIndex: null,
@@ -53,9 +66,26 @@
         }
       },
       addEvent(eventType) {
-        this.events.push({type: eventType, startTime: moment(), comment: ''})
-        this.selectEvent(this.events.length - 1);
-      }
+        let newEvent = {id: uuidv4(), type: eventType, startTime: moment(), comment: ''};
+        this.events.push(newEvent)
+        this.selectEvent(null);
+        createEvent(newEvent);
+      },
+      changeEvent(event, fieldName, value) {
+        event[fieldName] = value;
+        saveEvent(newEvent);
+      },
+      createEvent(event) {
+        console.log('createEvent(',JSON.stringify(event),')');
+      },
+      saveEvent(event) {
+        console.log('saveEvent(',JSON.stringify(event),')');
+      },
+      moment
+    },
+    created() {
+      console.log('created');
+      
     },
     computed: {
       eventTypes() {
@@ -63,7 +93,6 @@
         let types = [];
         for (let i = this.events.length - 1; i >= 0; --i) {
           let type = this.events[i].type;
-          console.log('t', type, 1);
           if (!typeSet.has(type)) {
             typeSet.add(type);
             types.push(type);
@@ -97,6 +126,9 @@
     padding: .5em;
     border: solid #aaa 1px;
   }
+  .event.dummy-event {
+    min-height: 3em;
+  }
   .event .row1 {
     display: flex;
     font-size: 130%;
@@ -104,8 +136,9 @@
   .event .row2 {
     display: flex;
   }
-  .event .row2 input {
+  .event  input {
     flex: 1;
+    font-size: 1em;
   }
   .event.selected {
     background-color: #eeeeee;
@@ -120,6 +153,7 @@
   .event-type-filter {
     display: flex;
     padding: .5em;
+    background-color: #000;
   }
   .event-type-filter input {
     flex: 1;
@@ -135,5 +169,6 @@
   .event-types .event-type {
     padding: .5em;
     border: solid #eee 1px;
+    font-size: 130%;
   }
 </style>
